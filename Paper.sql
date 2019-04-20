e6ab1119818bcf3e9528bdf2fcc0c36c81f30244
Xolvier <49654208+Xolvier@users.noreply.github.com>
2019-04-20 12:14:46 -0400

    Delete Paper.sql

--- Paper.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Paper
-(
-  Id       INT NOT NULL,       
-  Title            VARCHAR (100),  
-  Abstract          VARCHAR (1000), 
-  FileName          VARCHAR (100),     
-  PRIMARY KEY     (Id)                 
-);
