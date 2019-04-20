f44b8b67b4fa37d2587e89e9b173d19d3fa28e4e
Xolvier <49654208+Xolvier@users.noreply.github.com>
2019-04-20 12:14:52 -0400

    Delete Author.sql

--- Author.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Author
-(
-  EmailAddr       VARCHAR(100) NOT NULL,       
-  firstname            VARCHAR(50),  
-  LastName          VARCHAR(50),    
-  PRIMARY KEY     (EmailAddr)                  
-);
